286d612a855cc49885842f3ae545859e6c79f38d
deious <gkstjddlekd@gmail.com>
2023-06-19 23:21:39 +0900

    문제풀이

---
+++ Solved/BOJ/Main_bj_15652_N과M(4).java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+	static int N, R;
+	static int[] a, b;
+	
+	static StringBuilder sb = new StringBuilder();
+	
+	private static void perm(int cnt, int start) {
+
+		if (cnt == R) {
+			for (int i = 0; i < R; i++) {
+				sb.append(b[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = start; i <= N; i++) {
+			b[cnt] = i;
+			perm(cnt + 1, i);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		StringTokenizer st = new StringTokenizer(s);
+		N = Integer.parseInt(st.nextToken());
+		a = new int[N];
+		for (int i = 0; i < N; i++) {
+			a[i] = i + 1;
+		}
+		R = Integer.parseInt(st.nextToken());
+		b = new int[R];
+		
+		perm(0, 1);
+		
+		System.out.println(sb);
+		br.close();
+	}
+
+}
